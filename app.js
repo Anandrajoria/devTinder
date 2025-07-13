fd8d8b1c64ab629ab1caf5b41aa7f0511c95d2e7
Anandrajoriya <anandrajoriya03@gmail.com>
2025-07-13 19:13:22 +0530

    Implemented dynamic routing for user ID in Express

--- app.js
+++ app.js
@@ -7,46 +7,25 @@ longer routes saparated by / must me put at top of routes and
 wild card must stay at last
 */
 
-// this will only handle get call to /user
-//? get request to get data from database
-app.get("/user", (req, res) => {
-  res.send({
-    firstname: "adi",
-    lastname: "pra",
-  });
-});
+// ! dynamic routing
 
-//? post request for adding the data to databse
-app.post('/user',(req,res)=>{
-  console.log("send data to server");
-  res.send("data successfully saved to database")
-  
-})
-//? delete for deleting the data from databse
-app.delete('/user',(req,res)=>{
-  console.log("data deleted from server");
-  res.send("data successfully deleted form server")
-})
-
-
-//? put to  update or replace an entire resource.
-app.put('/user', (req, res) => {
-  console.log("data updated on server");
-  res.send("data successfully updated on server");
+app.get("/user/:userid/:bookid", (req, res) => {
+  const { userid, bookid } = req.params;
+  res.send(`User ID requested is ${userid} with book id ${bookid}`);
 });
 
 
-// ? patch To partially update a resource (only specific fields).
-app.patch('/user', (req, res) => {
-  console.log("data partially updated on server");
-  res.send("data partially updated on server");
-})
 
-
-//this will match aal the http method api call to /test
-app.use("/test", (req, res) => {
-  res.send("nameste aditya");
+// optional routing doesn't work in express_5 
+app.get("/user2/:userid", (req, res) => {
+  res.send(`user id is ${req.params.userid}`);
 });
+
+app.get("/user2", (req, res) => {
+  res.send("user id is not provided");
+});
+
+
 
 app.listen(7777, () => {
   console.log("server is running ");
