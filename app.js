fe39f1462c77af4e0da282209c73b8fe1796c48f
Anandrajoriya <anandrajoriya03@gmail.com>
2025-07-14 18:28:08 +0530

    conditional route handling

--- app.js
+++ app.js
@@ -2,31 +2,21 @@ const express = require("express");
 const app = express();
 
 
-// we can wrap routes inside array
-// !  app.use('/route',r1,[r2,r3],r4,r5)
-app.use("/user", [
-  (req, res, next) => {
-    // route handler
-    console.log("handleig the route");
-    // res.send("route handler 1");
-    next();
-  },
-  (req, res, next) => {
-    console.log("handling 2nd route");
-    // res.send("route handler 2");
-    next();
-  }],
-  (req, res, next) => {
-    console.log("handling 3rd route");
-    // res.send("route handeler 3");
-    next()
-  },
-  (req, res, next) => {
-    console.log("handling 4th route");
-    res.send("route handeler 4");
-    // next();
-  },
-);
+// conditional route handling
+
+app.get('/check/:age',(req,res,getGo)=>{
+  const age=parseInt(req.params.age)
+  if(age>=18){
+    console.log('adult');
+    getGo()
+  }else{
+    res.send('you are underage')
+  }
+},(req,res)=>{
+  res.send("welcome adult user")
+}
+)
+
 
 app.listen(7777, () => {
   console.log("server is running ");
