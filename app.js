ccf8c5905ba6178537c439e72018c54638eb03f8
Anandrajoriya <anandrajoriya03@gmail.com>
2025-07-14 18:15:05 +0530

    multiple route handler

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 const express = require("express");
 const app = express();
 
-/*
-! order of the routes matter 
-longer routes saparated by / must me put at top of routes and 
-wild card must stay at last
-*/
 
-// ! dynamic routing
-
-app.get("/user/:userid/:bookid", (req, res) => {
-  const { userid, bookid } = req.params;
-  res.send(`User ID requested is ${userid} with book id ${bookid}`);
-});
-
-
-
-// optional routing doesn't work in express_5 
-app.get("/user2/:userid", (req, res) => {
-  res.send(`user id is ${req.params.userid}`);
-});
-
-app.get("/user2", (req, res) => {
-  res.send("user id is not provided");
-});
-
-
+// we can wrap routes inside array
+// !  app.use('/route',r1,[r2,r3],r4,r5)
+app.use("/user", [
+  (req, res, next) => {
+    // route handler
+    console.log("handleig the route");
+    // res.send("route handler 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling 2nd route");
+    // res.send("route handler 2");
+    next();
+  }],
+  (req, res, next) => {
+    console.log("handling 3rd route");
+    // res.send("route handeler 3");
+    next()
+  },
+  (req, res, next) => {
+    console.log("handling 4th route");
+    res.send("route handeler 4");
+    // next();
+  },
+);
 
 app.listen(7777, () => {
   console.log("server is running ");
