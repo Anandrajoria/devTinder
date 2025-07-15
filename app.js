d195bfc58c5bd7a35332fffa729ac06777ccd26b
Anandrajoriya <anandrajoriya03@gmail.com>
2025-07-15 19:47:01 +0530

    connection between backend and database #4

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./src/config/database");
 
-const { adminAuth,userAuth } = require("./src/middleware/auth");
-app.use("/admin",adminAuth);
-app.use('/user/login',(req,res)=>{
-  res.send("user loggend in suuessfully")
-})
-app.use ("/user", userAuth,(req, res) => {
-  res.send("user data sent");
-});
-app.get("/admin/getAllData", (req, res) => {
-  res.send("all data sent");
-});
-app.get("/admin/delAUser", (req, res) => {
-  res.send("deleted a user");
-});
+connectDB()
+  .then(() => {
+    console.log("database connection successful");
+    app.listen(7777, () => {
+      console.log("server is running");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-app.listen(7777, () => {
-  console.log("server is running ");
-});
