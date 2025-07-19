08b9dd95f4312ea0eee07fb579d543353e4c687e
Anandrajoriya <anandrajoriya03@gmail.com>
2025-07-19 08:51:01 +0530

    got all user data from database and also use findOne method #8

--- app.js
+++ app.js
@@ -6,14 +6,33 @@ const User = require("./src/models/user");
 app.use(express.json());
 app.post("/signup", async (req, res) => {
   //!creating a instance of user model
-
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send("user added successfuly");
   } catch (err) {
     res.status(400).send("err saying user:" + err.message);
+  }
+});
+
+//get user by firstname findOne()
+app.get("/user", async (req, res) => {
+  const userFirstName = req.body.firstName;
+  try {
+    const users = await User.findOne({ firstName: userFirstName });
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//! feed api = GET /feed - get all the users form database
+app.get("/feed", async (sreq, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("something went wrong");
   }
 });
 
