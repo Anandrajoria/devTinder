f2f970bbe78ebc94fcc85e3c512e79c65a3932ab
Anandrajoriya <anandrajoriya03@gmail.com>
2025-07-20 11:02:59 +0530

    api level data sanitation and validation #11

--- app.js
+++ app.js
@@ -2,29 +2,72 @@ const express = require("express");
 const app = express();
 const connectDB = require("./src/config/database");
 const User = require("./src/models/user");
-// const user = require("./src/models/user");
-
+const { body, validationResult } = require("express-validator");
 app.use(express.json());
 
-app.post("/signup", async (req, res) => {
+const updateUserValidationRules = [
+  //validate age
+  body("age")
+    .optional()
+    .isInt({ min: 18 })
+    .withMessage("age must be number and at least 18."),
+
+  //validate skills
+  body("skills").optional().isArray().withMessage("skills must be an array"),
+
+  //validate photoUrl
+  body("photoUrl")
+    .optional()
+    .isURL()
+    .withMessage("Please provied a valid photo url"),
+
+  //gender validation
+  body(["about", "gender"]).optional().isString().trim(),
+];
+
+const signupValidationRules = [
+  body("email").isEmail().withMessage("Valid email is required."),
+  body("firstName")
+    .notEmpty()
+    .withMessage("Name is required.")
+    .isLength({ min: 4 })
+    .withMessage("First name must be at least 4 characters."),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters."),
+  body("gender")
+    .isIn(["male", "female", "other"])
+    .withMessage("Gender must be 'male', 'female' or 'other'."),
+];
+
+app.post("/signup", signupValidationRules, async (req, res) => {
   //!creating a instance of user model
-  const user = new User(req.body);
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
   try {
+    const user = new User(req.body);
     await user.save();
-    res.send("user added successfuly");
+    res.status(201).send({ message: "User added successfully." });
   } catch (err) {
-    res.status(400).send("err saying user:" + err.message);
+    res.status(400).send({ error: "Error saving user: " + err.message });
   }
 });
 
 // get user by email
 app.get("/user", async (req, res) => {
-  const userEmail = req.body.email;
+  const userEmail = req.query.email;
   try {
-    const user = await User.find({ email: userEmail });
+    const user = await User.findOne({ email: userEmail });
+    if (!user) {
+      return res.status(404).send({ error: "User not found." });
+    }
     res.send(user);
   } catch (error) {
-    res.status(500).send("something went wrong");
+    res.status(500).send({ error: "something went wrong." });
   }
 });
 
@@ -34,35 +77,67 @@ app.get("/feed", async (req, res) => {
     const feed = await User.find({});
     res.send(feed);
   } catch (error) {
-    res.status(500).send("something went wrong");
+    res.status(500).send({ error: "Something went wrong." });
   }
 });
 
 //delete api
-app.delete("/user",async (req,res)=>{
-  const userId=req.body.userId
+app.delete("/user/:id", async (req, res) => {
+  const userId = req.params.id;
   try {
-    await User.findByIdAndDelete({_id:userId})
-    await User.findByIdAndDelete(userId)
-    res.send("user deleted successfully")
+    // await User.findByIdAndDelete({ _id: userId });
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).send({ error: "User not found." });
+    }
+
+    res.send({ message: "User deleted successfully." });
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).send({ error: err.message });
   }
-})
+});
 
 //user data update api based on id
-app.patch("/user",async(req,res)=>{
-  const userId=req.body.userId
-  const data=req.body
-  try {
-    await User.findByIdAndUpdate({_id:userId},data)
-    runValidator:true
-    res.send("data updated  successfully")
-  } catch (error) {
-    res.status(500).send(error.message)
+app.patch("/user/:userId", updateUserValidationRules, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ error: errors.array() });
   }
-})
 
+  const userId = req.params.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATE = ["photoUrl", "about", "gender", "skills", "age"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATE.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      return res
+        .status(400)
+        .send({ error: "Update contains forbidden fields." });
+    }
+    if (data.skills && data.skills.length > 10) {
+      return res
+        .status(400)
+        .send({ error: "You can have a maximum of 10 skills." });
+    }
+    const updatedUser = await User.findByIdAndUpdate(userId, data, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedUser) {
+      return res.status(404).send({ error: "User not found." });
+    }
+
+    res.send(updatedUser);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 connectDB()
   .then(() => {
