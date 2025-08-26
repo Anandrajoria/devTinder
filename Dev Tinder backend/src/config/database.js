b7f932fc2ee99a2c1bb6cc1cba869172de186c97
Anandrajoriya <anandrajoriya03@gmail.com>
2025-08-26 15:04:55 +0530

    //

--- Dev Tinder backend/src/config/database.js
+++ Dev Tinder backend/src/config/database.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async  () => {
   
-  mongoose.connect(process.env.MONGO_URL);
+  // mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect("mongodb+srv://anandrajoriya03:Adi94144@namestenode.cuxwa6h.mongodb.net/?retryWrites=true&w=majority&appName=namesteNode");
 }
 
 module.exports = connectDB;
