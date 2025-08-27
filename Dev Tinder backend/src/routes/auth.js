a2fa6ae6eb4684b0e8726ce22b6577fbdf72be52
Anandrajoriya <anandrajoriya03@gmail.com>
2025-08-27 22:22:53 +0530

    login

--- Dev Tinder backend/src/routes/auth.js
+++ Dev Tinder backend/src/routes/auth.js
@@ -86,7 +86,7 @@ authRouter.post(
 );
 
 //login api
-authRouter.post("/login", async (req, res) => {
+authRouter.get("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password)
